10348dd6cffc1678c490b4871c2c291aaaaaadbd
Anna <yaroshenkoannette@gmail.com>
2024-12-10 00:30:42 +0100

    fix(no-rule): add alt assignment via js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.classList.add("button", "button_delete");
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.alt="delete button"
   deleteButtonImg.classList.add("button__icon")
   deleteButton.appendChild(deleteButtonImg);
 
